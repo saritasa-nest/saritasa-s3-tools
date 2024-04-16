e28b691d088513d114fa2a8041cf080ec131b0c7
Stanislav Khlud <stanislav.khlud@saritasa.com>
2024-04-16 12:14:35 +0700

    Init package

---
+++ invocations/project.py
@@ -0,0 +1,21 @@
+import invoke
+import saritasa_invocations
+
+
+@invoke.task
+def build(
+    context: invoke.Context,
+) -> None:
+    """Build python environ."""
+    saritasa_invocations.poetry.install(context)
+
+
+@invoke.task
+def init(
+    context: invoke.Context,
+) -> None:
+    """Prepare env for working with project."""
+    saritasa_invocations.git.setup(context)
+    saritasa_invocations.system.copy_vscode_settings(context)
+    build(context)
+    saritasa_invocations.docker.up(context)
