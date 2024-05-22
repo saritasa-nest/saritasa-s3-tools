18c3b1ca53841de7461f2b9c06b2b3b24629c7f9
Stanislav Khlud <stanislav.khlud@saritasa.com>
2024-05-22 14:45:49 +0700

    Improve plugin for buckets setup

--- saritasa_s3_tools/testing/plugin.py
+++ saritasa_s3_tools/testing/plugin.py
@@ -1,9 +1,12 @@
 import collections.abc
+import contextlib
 
 import pytest
 
 import botocore.credentials
+import botocore.exceptions
 import mypy_boto3_s3
+import mypy_boto3_s3.type_defs
 
 import saritasa_s3_tools
 
@@ -27,8 +30,9 @@ def pytest_addoption(parser: pytest.Parser) -> None:
         "Region for s3.",
     )
     parser.addini(
-        "s3_bucket",
+        "s3_bucket_name",
         "Bucket for s3.",
+        default="saritasa-s3-tools",
     )
 
 
@@ -107,36 +111,137 @@ def boto3_client(
 
 
 @pytest.fixture(scope="session")
-def s3_bucket(
+def s3_bucket_name(
     request: pytest.FixtureRequest,
 ) -> str:
     """Get the name of s3 bucket."""
-    if bucket := request.config.inicfg.get("s3_bucket", ""):
-        return str(bucket)
-    raise NotImplementedError(  # pragma: no cover
-        "Please set up `s3_bucket` fixture",
+    worker_input = getattr(
+        request.config,
+        "workerinput",
+        {
+            "workerid": "",
+        },
     )
+    return "-".join(
+        filter(
+            None,
+            (
+                str(
+                    request.config.inicfg.get(
+                        "s3_bucket_name",
+                        "saritasa-s3-tools",
+                    ),
+                ),
+                worker_input["workerid"],
+            ),
+        ),
+    )
+
+
+@pytest.fixture(scope="session")
+def s3_bucket_cleaner(
+    boto3_client: mypy_boto3_s3.S3Client,
+) -> collections.abc.Callable[[str], None]:
+    """Get bucket cleaner."""
+
+    def _clean(bucket: str) -> None:
+        s3_objects: list[mypy_boto3_s3.type_defs.ObjectIdentifierTypeDef] = [
+            {"Key": s3_object.get("Key", "")}
+            for s3_object in boto3_client.list_objects_v2(Bucket=bucket).get(
+                "Contents",
+                [],
+            )
+            if s3_object.get("Key", "")
+        ]
+        if not s3_objects:
+            return
+        boto3_client.delete_objects(
+            Bucket=bucket,
+            Delete={
+                "Objects": s3_objects,
+            },
+        )
+
+    return _clean
+
+
+@pytest.fixture(scope="session")
+def s3_bucket_factory(
+    boto3_client: mypy_boto3_s3.S3Client,
+    s3_region: str,
+    s3_bucket_cleaner: collections.abc.Callable[[str], None],
+) -> collections.abc.Callable[
+    [str],
+    contextlib._GeneratorContextManager[
+        mypy_boto3_s3.type_defs.CreateBucketOutputTypeDef
+    ],
+]:
+    """Get factory for generating s3 buckets."""
+
+    @contextlib.contextmanager
+    def _create(
+        bucket: str,
+    ) -> collections.abc.Generator[
+        mypy_boto3_s3.type_defs.CreateBucketOutputTypeDef,
+        None,
+        None,
+    ]:
+        with contextlib.suppress(botocore.exceptions.ClientError):
+            boto3_client.head_bucket(Bucket=bucket)
+            s3_bucket_cleaner(bucket)
+            boto3_client.delete_bucket(Bucket=bucket)
+        yield boto3_client.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={
+                "LocationConstraint": s3_region,  # type: ignore
+            },
+        )
+        s3_bucket_cleaner(bucket)
+        boto3_client.delete_bucket(Bucket=bucket)
+
+    return _create
+
+
+@pytest.fixture(scope="session")
+def s3_bucket(
+    s3_bucket_factory: collections.abc.Callable[
+        [str],
+        contextlib._GeneratorContextManager[
+            mypy_boto3_s3.type_defs.CreateBucketOutputTypeDef
+        ],
+    ],
+    s3_bucket_name: str,
+) -> collections.abc.Generator[
+    mypy_boto3_s3.type_defs.CreateBucketOutputTypeDef,
+    None,
+    None,
+]:
+    """Create s3 bucket."""
+    with s3_bucket_factory(s3_bucket_name) as bucket:
+        yield bucket
 
 
 @pytest.fixture(scope="session")
 def s3_client(
     boto3_client: mypy_boto3_s3.S3Client,
-    s3_bucket: str,
+    s3_bucket_name: str,
+    s3_bucket: mypy_boto3_s3.type_defs.CreateBucketOutputTypeDef,
 ) -> saritasa_s3_tools.S3Client:
     """Set up s3 client."""
     return saritasa_s3_tools.S3Client(
         boto3_client=boto3_client,
-        default_bucket=s3_bucket,
+        default_bucket=s3_bucket_name,
     )
 
 
 @pytest.fixture(scope="session")
 def async_s3_client(
     boto3_client: mypy_boto3_s3.S3Client,
-    s3_bucket: str,
+    s3_bucket_name: str,
+    s3_bucket: mypy_boto3_s3.type_defs.CreateBucketOutputTypeDef,
 ) -> saritasa_s3_tools.S3Client:
     """Set up async s3 client."""
     return saritasa_s3_tools.AsyncS3Client(
         boto3_client=boto3_client,
-        default_bucket=s3_bucket,
+        default_bucket=s3_bucket_name,
     )
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -3,31 +3,46 @@ import botocore.credentials
 import saritasa_s3_tools
 
 
-def test_s3_file_factory(s3_client: saritasa_s3_tools.S3Client) -> None:
+def test_s3_file_factory(
+    s3_client: saritasa_s3_tools.S3Client,
+    s3_bucket_name: str,
+) -> None:
     """Test S3FileField."""
     file_key = saritasa_s3_tools.factory.S3FileField(
         s3_config="files",
         s3_region="us-west-1",
-        bucket="saritasa-s3-tools-files",
+        bucket=s3_bucket_name,
         access_key_getter=lambda: botocore.credentials.Credentials(
             access_key="root",
             secret_key="rootroot",
         ),
         s3_endpoint_url_getter=lambda: "https://localhost.localstack.cloud:4566",
     ).evaluate(object(), None, None)
-    s3_client.is_file_in_bucket(file_key), file_key
+    (
+        s3_client.is_file_in_bucket(
+            file_key,
+            bucket=s3_bucket_name,
+        ),
+        file_key,
+    )
 
 
-def test_s3_image_factory(s3_client: saritasa_s3_tools.S3Client) -> None:
+def test_s3_image_factory(
+    s3_client: saritasa_s3_tools.S3Client,
+    s3_bucket_name: str,
+) -> None:
     """Test S3ImageFileField."""
     file_key = saritasa_s3_tools.factory.S3ImageFileField(
         s3_config="files",
         s3_region="us-west-1",
-        bucket="saritasa-s3-tools-files",
+        bucket=s3_bucket_name,
         access_key_getter=lambda: botocore.credentials.Credentials(
             access_key="root",
             secret_key="rootroot",
         ),
         s3_endpoint_url_getter=lambda: "https://localhost.localstack.cloud:4566",
     ).evaluate(object(), None, None)
-    assert s3_client.is_file_in_bucket(file_key), file_key
+    assert s3_client.is_file_in_bucket(
+        file_key,
+        bucket=s3_bucket_name,
+    ), file_key
